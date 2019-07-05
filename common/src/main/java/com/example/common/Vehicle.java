a4d895c49e128df14980c229e60157aa9c04f5b2
Sabby Anandan <sanandan@pivotal.io>
2019-07-05 11:46:11 -0700

    Initial commit

---
+++ common/src/main/java/com/example/common/Vehicle.java
@@ -0,0 +1,79 @@
+package com.example.common;
+
+/**
+ * Created by sanandan on 7/3/19.
+ */
+public class Vehicle {
+
+	String VIN;
+
+	Manufacturer manufacturer;
+
+	VehicleType type;
+
+	ScheduleStartTime startTime;
+
+	public enum VehicleType {
+		CAR, SUV, TRUCK;
+	}
+
+	public enum Manufacturer {
+		TESLA, LANDROVER, BMW;
+	}
+
+	public enum ScheduleStartTime {
+		T_0600, T_0605, T_0610, T_0630, T_0705, T_0710, T_0720, T_0730, T_0735, T_0739, T_0800;
+	}
+
+	public Vehicle() {
+	}
+
+	public Vehicle(String VIN, Manufacturer manufacturer, VehicleType type,
+			ScheduleStartTime startTime) {
+		this.VIN = VIN;
+		this.manufacturer = manufacturer;
+		this.type = type;
+		this.startTime = startTime;
+	}
+
+	public String getVIN() {
+		return VIN;
+	}
+
+	public void setVIN(String VIN) {
+		this.VIN = VIN;
+	}
+
+	public Manufacturer getManufacturer() {
+		return manufacturer;
+	}
+
+	public void setManufacturer(Manufacturer manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+
+	public VehicleType getType() {
+		return type;
+	}
+
+	public void setType(VehicleType type) {
+		this.type = type;
+	}
+
+	public ScheduleStartTime getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(ScheduleStartTime startTime) {
+		this.startTime = startTime;
+	}
+
+	@Override public String toString() {
+		return "Vehicle{" +
+				"VIN='" + VIN + '\'' +
+				", manufacturer=" + manufacturer +
+				", type=" + type +
+				", startTime=" + startTime +
+				'}';
+	}
+}
---
+++ vehicle/src/main/java/com/example/vehicle/VehicleApplication.java
@@ -0,0 +1,56 @@
+package com.example.vehicle;
+
+import com.example.common.Vehicle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+@SpringBootApplication
+@EnableScheduling
+@EnableBinding(Source.class)
+public class VehicleApplication {
+
+	static List<Vehicle.VehicleType> vehicles = Arrays
+			.asList(Vehicle.VehicleType.CAR, Vehicle.VehicleType.SUV, Vehicle.VehicleType.TRUCK);
+
+	static List<Vehicle.Manufacturer> manufacturers = Arrays
+			.asList(Vehicle.Manufacturer.LANDROVER, Vehicle.Manufacturer.BMW, Vehicle.Manufacturer.TESLA);
+
+	static List<Vehicle.ScheduleStartTime> startTimes = Arrays
+			.asList(Vehicle.ScheduleStartTime.T_0600, Vehicle.ScheduleStartTime.T_0605,
+					Vehicle.ScheduleStartTime.T_0610,
+					Vehicle.ScheduleStartTime.T_0710, Vehicle.ScheduleStartTime.T_0705,
+					Vehicle.ScheduleStartTime.T_0630,
+					Vehicle.ScheduleStartTime.T_0720, Vehicle.ScheduleStartTime.T_0730,
+					Vehicle.ScheduleStartTime.T_0739,
+					Vehicle.ScheduleStartTime.T_0800, Vehicle.ScheduleStartTime.T_0735,
+					Vehicle.ScheduleStartTime.T_0605, Vehicle.ScheduleStartTime.T_0630,
+					Vehicle.ScheduleStartTime.T_0739);
+
+	@Autowired
+	private Source source;
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleApplication.class, args);
+	}
+
+	@Scheduled(fixedRate = 2000L)
+	public void generateAndSendNewVehicle() {
+		Vehicle vehicle = new Vehicle(Long.toHexString(Double.doubleToLongBits(Math.random())),
+				manufacturers.get(new Random().nextInt(manufacturers.size())),
+				vehicles.get(new Random().nextInt(vehicles.size())),
+				startTimes.get(new Random().nextInt(startTimes.size())));
+
+		source.output().send(MessageBuilder.withPayload(vehicle).build());
+		System.out.println("Generated Vehicle: " + vehicle);
+	}
+}
