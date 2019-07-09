396b5b5e20693f387c00c1b12c6cdbe11ef2238a
Sabby Anandan <sanandan@pivotal.io>
2019-07-08 18:51:51 -0700

    Cleanup

--- schedule/src/main/java/com/example/schedule/ScheduleApplication.java
+++ schedule/src/main/java/com/example/schedule/ScheduleApplication.java
@@ -3,7 +3,7 @@ package com.example.schedule;
 import com.example.common.Vehicle;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.Serialized;
 import org.apache.kafka.streams.kstream.TimeWindows;
 import org.apache.kafka.streams.kstream.Windowed;
 import org.apache.kafka.streams.state.KeyValueIterator;
@@ -16,6 +16,7 @@ import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.Input;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.binder.kafka.streams.InteractiveQueryService;
+import org.springframework.kafka.support.serializer.JsonSerde;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,14 +33,54 @@ public class ScheduleApplication {
 		SpringApplication.run(ScheduleApplication.class, args);
 	}
 
-	@StreamListener("input")
-	public void get(KStream<String, Vehicle> input) {
+	/*@StreamListener("input")
+	public void getOne(KStream<String, Vehicle> input) {
 
 		input
 				.map((k, v) -> new KeyValue<>(((Vehicle) v).getStartTime(), ((Vehicle) v)))
-				.groupBy((k, v) -> new KeyValue<>(((Vehicle) v).getStartTime(), ((Vehicle) v)))
+				.groupByKey(Serialized
+						.with(new JsonSerde<>(Vehicle.ScheduleStartTime.class), new JsonSerde<>(Vehicle.class)))
 				.windowedBy(TimeWindows.of(10000))
-				.count(Materialized.as("foo-v5"))
+				.count(Materialized.as("foo-u5"))
+				.toStream()
+				.map((k, v) -> {
+					System.out.println("In the last " + 30 + " secs, " + v
+							+ " new Vehicles were added to the " + k
+							+ " Start-Time bucket.");
+					return new KeyValue(k, v);
+
+		});
+	}*/
+
+	/*@StreamListener("input")
+	public void getTwo(KStream<String, Vehicle> input) {
+
+		input
+				.map((k, v) -> new KeyValue<>(((Vehicle) v).getStartTime(), ((Vehicle) v)))
+				.groupByKey(Serialized
+						.with(new JsonSerde<>(Vehicle.ScheduleStartTime.class), new JsonSerde<>(Vehicle.class)))
+				.windowedBy(TimeWindows.of(10000))
+				.aggregate(Vehicle::new, (scheduleStartTime, vehicle, vehicle2) -> {
+					return vehicle2;
+				})
+				.toStream()
+				.map((k, v) -> {
+					System.out.println("In the last " + 30 + " secs, " + v
+							+ " new Vehicles were added to the " + k
+							+ " Start-Time bucket.");
+					return new KeyValue(k, v);
+				});
+	}*/
+
+	@StreamListener("input")
+	public void getThree(KStream<String, Vehicle> input) {
+
+		input
+				.map((k, v) -> new KeyValue<>(((Vehicle) v).getStartTime(), ((Vehicle) v)))
+				.groupByKey(Serialized
+						.with(new JsonSerde<>(Vehicle.ScheduleStartTime.class), new JsonSerde<>(Vehicle.class)))
+				.windowedBy(TimeWindows.of(10000))
+				.reduce((vehicle, v1) -> v1)
 				.toStream()
 				.map((k, v) -> {
 					System.out.println("In the last " + 30 + " secs, " + v
