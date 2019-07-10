585e1ce8aae800443b01884fafa8bf9eaa68deba
Soby Chacko <schacko@pivotal.io>
2019-07-10 19:28:07 -0400

    delegate to json deserializer

--- schedule/src/main/java/com/example/schedule/VehiclesSerde.java
+++ schedule/src/main/java/com/example/schedule/VehiclesSerde.java
@@ -1,28 +1,24 @@
 package com.example.schedule;
 
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.util.Date;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 import com.example.common.Vehicle;
 import org.apache.kafka.common.serialization.Deserializer;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serializer;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 public class VehiclesSerde implements Serde<Vehicles> {
 	@Override
-	public void configure(Map<String, ?> configs, boolean isKey) {
-
-	}
+	public void configure(Map<String, ?> configs, boolean isKey) {}
 
 	@Override
-	public void close() {
-
-	}
+	public void close() {}
 
 	@Override
 	public Serializer<Vehicles> serializer() {
@@ -33,7 +29,7 @@ public class VehiclesSerde implements Serde<Vehicles> {
 
 			@Override
 			public byte[] serialize(final String s, final Vehicles vehicles) {
-
+				JsonSerializer<Vehicle> jsonSerializer = new JsonSerializer<>();
 				final ByteArrayOutputStream out = new ByteArrayOutputStream();
 				final DataOutputStream
 						dataOutputStream =
@@ -41,12 +37,8 @@ public class VehiclesSerde implements Serde<Vehicles> {
 				try {
 
 					for (Vehicle vehicle : vehicles) {
-
-						dataOutputStream.writeUTF(vehicle.getVIN());
-						dataOutputStream.writeUTF(vehicle.getManufacturer().name());
-						dataOutputStream.writeUTF(vehicle.getType().name());
-						dataOutputStream.writeUTF(vehicle.getStartTime().name());
-						dataOutputStream.writeLong(vehicle.getScheduledTime().getTime());
+						byte[] serialized = jsonSerializer.serialize(s, vehicle);
+						dataOutputStream.write(serialized);
 					}
 					dataOutputStream.flush();
 				} catch (IOException e) {
@@ -66,85 +58,27 @@ public class VehiclesSerde implements Serde<Vehicles> {
 	public Deserializer<Vehicles> deserializer() {
 		return new Deserializer<Vehicles>() {
 			@Override
-			public void configure(final Map<String, ?> map, final boolean b) {
-
-			}
+			public void configure(final Map<String, ?> map, final boolean b) {}
 
 			@Override
 			public Vehicles deserialize(final String s, final byte[] bytes) {
-				if (bytes == null || bytes.length == 0) {
+				if (bytes.length == 0) {
 					return null;
 				}
+				String stringBytes = new String(bytes, StandardCharsets.UTF_8);
+				String[] splits = stringBytes.split("(?<=})");
 				final Vehicles result = new Vehicles();
-
-				final DataInputStream
-						dataInputStream =
-						new DataInputStream(new ByteArrayInputStream(bytes));
-
-				try {
-					while(dataInputStream.available() > 0) {
-						String vin = dataInputStream.readUTF();
-						String s1 = dataInputStream.readUTF();
-						Vehicle.Manufacturer manufacturer = null;
-						switch (s1) {
-							case "TESLA": manufacturer = Vehicle.Manufacturer.TESLA;
-								break;
-							case "BMW": manufacturer = Vehicle.Manufacturer.BMW;
-								break;
-							case "LANDROVER": manufacturer = Vehicle.Manufacturer.LANDROVER;
-								break;
-						}
-						String s2 =dataInputStream.readUTF();
-						Vehicle.VehicleType vehicleType = null;
-						switch (s2) {
-							case "CAR": vehicleType = Vehicle.VehicleType.CAR;
-								break;
-							case "SUV": vehicleType =  Vehicle.VehicleType.SUV;
-								break;
-							case "TRUCK": vehicleType =  Vehicle.VehicleType.TRUCK;
-								break;
-						}
-						String s3 = dataInputStream.readUTF();
-
-						Vehicle.ScheduleStartTime scheduleStartTime = null;
-						switch (s3) {
-							case "T_0600": scheduleStartTime = Vehicle.ScheduleStartTime.T_0600;
-								break;
-							case "T_0605": scheduleStartTime = Vehicle.ScheduleStartTime.T_0605;
-								break;
-							case "T_0610": scheduleStartTime = Vehicle.ScheduleStartTime.T_0610;
-								break;
-							case "T_0630": scheduleStartTime = Vehicle.ScheduleStartTime.T_0630;
-								break;
-							case "T_0705": scheduleStartTime = Vehicle.ScheduleStartTime.T_0705;
-								break;
-							case "T_0710": scheduleStartTime = Vehicle.ScheduleStartTime.T_0710;
-								break;
-							case "T_0720": scheduleStartTime = Vehicle.ScheduleStartTime.T_0720;
-								break;
-							case "T_0730": scheduleStartTime = Vehicle.ScheduleStartTime.T_0730;
-								break;
-							case "T_0735": scheduleStartTime = Vehicle.ScheduleStartTime.T_0735;
-								break;
-							case "T_0739": scheduleStartTime = Vehicle.ScheduleStartTime.T_0739;
-								break;
-							case "T_0800": scheduleStartTime = Vehicle.ScheduleStartTime.T_0800;
-								break;
-						}
-						Date schedule = new Date(dataInputStream.readLong());
-
-						result.add(new Vehicle(vin, manufacturer, vehicleType, scheduleStartTime, schedule));
-					}
-				} catch (IOException e) {
-					throw new RuntimeException(e);
+				JsonDeserializer<Vehicle> jsonDeserializer = new JsonDeserializer<>(Vehicle.class);
+				for (String split : splits) {
+					Vehicle deserialized = jsonDeserializer.deserialize(s, split.getBytes());
+					result.list.add(deserialized);
 				}
+
 				return result;
 			}
 
 			@Override
-			public void close() {
-
-			}
+			public void close() {}
 		};
 	}
 }
